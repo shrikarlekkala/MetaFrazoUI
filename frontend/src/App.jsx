b51aaa0c86a0e5115102ce1cf12feb16942b843f
harshit960 <raj.harshit960@gmail.com>
2024-08-27 07:34:14 +0530

    chore: Add initial project files and configurations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./section/Hero";
+import Products from "./section/Products";
+
+export default function App() {
+  return (
+    <>
+    <Hero/>
+    <Products/>
+    
+    </>
+  )
+}
