2fac39ef8652dc44251a0604bb5d627f2d6c3a68
harshit960 <raj.harshit960@gmail.com>
2024-09-02 23:59:12 +0530

    refactor: Update CTA and Email sections layout and styling

--- frontend/src/sections/CTA.jsx
+++ frontend/src/sections/CTA.jsx
@@ -4,7 +4,7 @@ import CTABtn from '../components/CTABtn'
 function CTA() {
     return (
         <>
-            <div className=" bg-[#F0F3F6] h-screen w-full flex flex-col items-center justify-center">
+            <div className=" bg-[#F0F3F6] h-[757px] w-full flex flex-col items-center justify-center">
                 <div className="text-[56px] z-20 font-bold my-6 text-center leading-[67px]">Unlock the power of AI to translate <br /> your video materials</div>
                 <div className="text-[20px] z-20 text-center">
                     Try first video translation tool that you’ll love. And the last one you’ll ever need.
@@ -12,7 +12,7 @@ function CTA() {
                 <div className="mt-6 z-20">
                     <CTABtn />
                 </div>
-                <img src="assets/BG.png" alt="" srcset="" className='absolute' />
+                <img src="assets/BG.png" alt="" srcset="" className='absolute max-h-full w-full' />
             </div>
         </>)
 }
--- frontend/src/sections/Email.jsx
+++ frontend/src/sections/Email.jsx
@@ -4,7 +4,7 @@ import EmailBar from '../components/EmailBar'
 function Email() {
     return (
         <>
-            <div className=" bg-[#F0F3F6] h-screen w-full flex flex-col items-center justify-center">
+            <div className=" bg-[#F0F3F6] h-[757px] w-full flex flex-col items-center justify-center">
                 <div className="text-[56px] font-bold my-6 text-center leading-[67px]">Want to hear from us or need <br /> more info?</div>
                 <div className="text-[20px] text-center">
                     Drop your email here, and we’ll get back to you with the latest <br /> updates and any help you need!
