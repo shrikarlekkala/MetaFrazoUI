d6623cd5ae48e3a2d2022582504fca80d44ee475
harshit960 <raj.harshit960@gmail.com>
2024-09-02 20:47:53 +0530

    refactor: Update ShowReel section layout and styling

--- frontend/src/sections/ShowReel.jsx
+++ frontend/src/sections/ShowReel.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function ShowReel() {
     return (
         <>
-            <div className="w-full h-screen bg-[#0A47F2] z-20 flex items-center justify-center">
-                <div className="w-4/5 h-4/5 bg-[#fff] rounded-[32px] ">
+            <div className="w-full p-[120px] bg-[#0A47F2] z-20 flex items-center justify-center">
+                <div className="w-full h-[684px] bg-[#fff] rounded-[32px] ">
                 </div>
             </div>
         </>
