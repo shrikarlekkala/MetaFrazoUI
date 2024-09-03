c1991e254eee324bb27b9489e6ce7b01aea5851c
harshit960 <raj.harshit960@gmail.com>
2024-09-03 16:34:08 +0530

    refactor: Update Question component UI and add toggle functionality

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Question() {
+    const [height, setheight] = useState("h-[0px] scale-0");
+    const handleClick = () => {
+        setheight("h-[296px] scale-100");
+    }
+
     return (
         <>
-            <div className="w-4/5 max-w-[1104px] bg-white h-[78px] rounded-[32px] flex items-center justify-between px-6">
-                <div className="text-[20px] font-bold">Question</div>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect x="11" width="2" height="24" rx="1" fill="#0E0E0C" />
-                    <rect x="24" y="11" width="2" height="24" rx="1" transform="rotate(90 24 11)" fill="#0E0E0C" />
-                </svg>
+            <div className={"flex-col w-4/5 max-w-[1104px] bg-white  rounded-[32px] flex items-center justify-between px-6 duration-200"}>
+                <div className="flex items-center justify-between w-full h-[78px]">
+
+                    <div className="text-[20px] font-bold">Question</div>
+                    <svg onClick={handleClick} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="11" width="2" height="24" rx="1" fill="#0E0E0C" />
+                        <rect x="24" y="11" width="2" height="24" rx="1" transform="rotate(90 24 11)" fill="#0E0E0C" />
+                    </svg>
+                </div>
+                <div className={height + " w-full duration-200"}>
+                    
+                </div>
 
             </div>
         </>)
